4ceb92beb9eda4f25b13c3332e636294d92aa983
samuelivanaskas <106975862+samuelivanaskas@users.noreply.github.com>
2023-05-09 15:18:00 -0300

    Update index.tsx

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -4,17 +4,15 @@ import { Campos } from '../../styles'
 import * as S from './styles'
 
 const BarraLateral = () => (
-  <S.Aside>
-    <Campos>
-      <input type="text" placeholder="Buscar contato" />
-      <S.Filtros>
-        <FiltroCard legenda="contatos não salvos" contador={1} />
-        <FiltroCard legenda="contatos salvos" contador={2} />
-        <FiltroCard legenda="contatos importantes" contador={3} />
-        <FiltroCard legenda="contatos urgentes" contador={4} ativo />
-      </S.Filtros>
-    </Campos>
-  </S.Aside>
+  <Campos>
+    <input type="text" placeholder="Buscar contato" />
+    <S.Filtros>
+      <FiltroCard legenda="contatos não salvos" contador={1} />
+      <FiltroCard legenda="contatos salvos" contador={2} />
+      <FiltroCard legenda="contatos importantes" contador={3} />
+      <FiltroCard legenda="contatos urgentes" contador={4} ativo />
+    </S.Filtros>
+  </Campos>
 )
 
 export default BarraLateral
